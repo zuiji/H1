bf47d27cbcefc87754e510e9bd857714c1aa426a
zuiji <32148833+zuiji@users.noreply.github.com>
2020-02-14 08:09:25 +0100

    Added comments

--- FirstInFirstOut/Guest.cs
+++ FirstInFirstOut/Guest.cs
@@ -4,10 +4,11 @@ namespace FirstInFirstOut
 {
     public class Guest
     {
+        //properties that are able to get the data from and set data in.
         public string Name { get; set; }
         public byte Age { get; set; }
  
-
+        //my constructor that needs augments name and age.
         public Guest(string name, byte age)
         {
             Name = name;
--- FirstInFirstOut/Logic.cs
+++ FirstInFirstOut/Logic.cs
@@ -7,22 +7,22 @@ namespace FirstInFirstOut
 
     public class Logic
     {
-
+        //Queue that added guest to the Queue
         public void AddGuest(Queue<Guest> guests, string name, byte age)
         {
             guests.Enqueue(new Guest(name, age));
         }
 
+        //returns the amount of guests in the Queue
         public int ShowTheNumberOfGuests(Queue<Guest> guests)
         {
             return guests.Count;
         }
 
+        //returns the lowest age from the Queue og guests
         public byte ShowMinAgeOnGuests(Queue<Guest> guests, byte minAge)
         {
             minAge = guests.Peek().Age;
-
-
             foreach (Guest guest in guests)
             {
 
@@ -31,15 +31,13 @@ namespace FirstInFirstOut
                     minAge = guest.Age;
                 }
             }
-
             return minAge;
         }
-
+        
+        //returns the highest age from the Queue og guests
         public byte ShowMaxAgeOnGuests(Queue<Guest> guests, byte maxAge)
         {
             maxAge = guests.Peek().Age;
-
-
             foreach (Guest guest in guests)
             {
 
@@ -48,10 +46,10 @@ namespace FirstInFirstOut
                     maxAge = guest.Age;
                 }
             }
-
             return maxAge;
         }
 
+        //finding a guest in the Queue
         public string FindAGuest(Queue<Guest> guests, string name)
         {
             name = guests.Peek().Name;
@@ -66,10 +64,10 @@ namespace FirstInFirstOut
                     return "Guest not found on the list";
                 }
             }
-
             return name;
         }
 
+        //printing out all from the list
         public void PrintAllItems(Queue<Guest> guests)
         {
             foreach (Guest guest in guests)
@@ -78,6 +76,7 @@ namespace FirstInFirstOut
             }
         }
 
+        //exit
         public void Exit()
         {
             Console.WriteLine($"Sorry to see you leave GoodBye");
